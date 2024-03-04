e11b1e50cd4c3bcd6da0effe4c7d42803672b562
iDobryDen <59179395+iDobryDen@users.noreply.github.com>
2024-03-04 06:19:46 +0400

    HW 3.7 done!

--- CSharpEducation/Task3.7/Program.cs
+++ CSharpEducation/Task3.7/Program.cs
@@ -1,10 +1,31 @@
-﻿namespace Task3._7;
+﻿using System;
 
-class Program
+namespace Task3._7_HW
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello, World!");
+        static void Main(string[] args)
+        {
+            int num1, num2;
+
+            Console.WriteLine("Введите первое число:");
+            num1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите второе число:");
+            num2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Первое число: {num1}, Второе число: {num2}");
+
+            SwapNumbers(ref num1, ref num2);
+
+            Console.WriteLine($"Числа после обмена: Первое - {num1}, Второе - {num2}");
+        }
+
+        public static void SwapNumbers(ref int num1, ref int num2)
+        {
+            int temp = num1;
+            num1 = num2;
+            num2 = temp;
+        }
     }
 }
-
