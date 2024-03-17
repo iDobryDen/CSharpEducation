1c2c8f78daa23b5bb3c515565f09a39877b085b9
iDobryDen <59179395+iDobryDen@users.noreply.github.com>
2024-03-17 19:52:54 +0400

    Task Prictice.5.5 done!

---
+++ CSharpEducation/Practice.5.5/Account.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace Practice._5._5
+{
+	public class Account
+	{
+		private static int _balance = 0;
+
+		public Account()
+		{
+
+		}
+
+		public void AddMoney(int addmoney)
+		{
+			_balance += addmoney;
+		}
+        public void TakeMoney(int takemoney)
+        {
+            _balance -= takemoney;
+        }
+        public void ShowBalance()
+        {
+            Console.WriteLine("Баланс: " + _balance);
+        }
+
+
+    }
+}
+
--- CSharpEducation/Practice.5.5/Program.cs
+++ CSharpEducation/Practice.5.5/Program.cs
@@ -1,10 +1,18 @@
-﻿namespace Practice._5._5;
+﻿//Создайте в папке проекта файл Account.cs, и создайте в нём новый public класс Account.Добавьте в класс поле _balance.
+//Поле пометьте модификатором private. Добавьте и реализуйте три метода: AddMoney, TakeMoney, ShowBalance.
+//Метод AddMoney увеличивает значение _balance на указанное значение. Метод TakeMoney уменьшает значение _balance на указанную сумму.
+//Метод ShowBalance возвращает значение _balance. Учтите, что ни в одном из методов не должно быть работы с консолью.
+namespace Practice._5._5;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Account account = new Account();
+
+        account.AddMoney(20);
+        account.TakeMoney(5);
+        account.ShowBalance();
     }
 }
 
