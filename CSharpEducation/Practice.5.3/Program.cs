c0c5145f6e7f9a166e329b5351193e094628e078
iDobryDen <59179395+iDobryDen@users.noreply.github.com>
2024-03-17 10:08:45 +0400

    practice5.3 done!

--- CSharpEducation/Practice.5.3/Program.cs
+++ CSharpEducation/Practice.5.3/Program.cs
@@ -5,7 +5,8 @@
 //а)конструктор без аргументов, который задаёт значения по умолчанию для указанных полей;
 //б)конструктор с аргументами, соответствующими полям класса;
 //с)конструктор, принимающий только 2 аргумента: имя и возраст.Остальные параметры заполните значениями по умолчанию.
-
+//Создайте в класса Program три объекта тремя разными конструкторами.
+//Для каждого из  объектов выведите на экран консоли значения их полей.
 
 class Program
 {
@@ -20,6 +21,16 @@ class Program
         //Den.Company = "UdGU";
 
         Console.WriteLine($"Firstname: {Den.Firstname} | Lastname: {Den.Lastname} | Age: {Den.Age} | Company: {Den.Company}");
+
+        // Создаем объекты с помощью разных конструкторов
+        Person student1 = new Person("Denis", 35);
+        Person student2 = new Person("Sonya", 35);
+        Person student3 = new Person("Anna", 10);
+
+        // Выводим на консоль значения полей каждого объекта
+        Console.WriteLine($"Firstname: {student1.Firstname} | Age: {student1.Age} | Company: {student1.Company}");
+        Console.WriteLine($"Firstname: {student2.Firstname} | Age: {student2.Age} | Company: {student2.Company}");
+        Console.WriteLine($"Firstname: {student3.Firstname} | Age: {student3.Age} | Company: {student3.Company}");
     }
 }
 
