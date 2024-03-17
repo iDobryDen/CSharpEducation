b0cbe4a250e6fff849f56cbcec9a65d1efaa20a0
iDobryDen <59179395+iDobryDen@users.noreply.github.com>
2024-03-17 20:23:57 +0400

    Create HomeWork.5

---
+++ CSharpEducation/HW 5.1-8/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace HW_5._1_8;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
+
---
+++ CSharpEducation/HW 5.1-8/Student.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace HW_5._1_8
+{
+	public class Student
+	{
+		public Student()
+		{
+		}
+	}
+}
+
