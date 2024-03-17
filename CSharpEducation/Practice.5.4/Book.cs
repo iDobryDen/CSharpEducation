bba92e036b6eb827e1ad13b50004e004f4723cf9
iDobryDen <59179395+iDobryDen@users.noreply.github.com>
2024-03-17 14:04:04 +0400

    Practice.5.4 done!

---
+++ CSharpEducation/Practice.5.4/Book.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace Practice._5._4
+{
+	public class Book
+	{
+		public static int BooksCount = 0;
+
+		public string BookName;
+		public string BookAuthor;
+		public int BookPublicYear;
+		public string BookGenre;
+
+		public Book(string name, string author, int year, string genre)
+		{
+            BookName = name;
+            BookAuthor = author;
+            BookPublicYear = year;
+            BookGenre = genre;
+
+            BooksCount++;
+		}
+	}
+}
+
--- CSharpEducation/Practice.5.4/Program.cs
+++ CSharpEducation/Practice.5.4/Program.cs
@@ -1,10 +1,17 @@
-﻿namespace Practice._5._4;
+﻿//* Создайте в папке проекта файл Book.cs, и создайте в нём новый public класс Book. В класс добавьте static поле BooksCount,
+//которое будет использоваться для подсчёта созданных книг. Создайте конструктор класса, в котором увеличивайте значение BooksCount на единицу.
+//В класс добавьте поля, соответствующие названию книги, автору книги, год издания, жанр. Создайте несколько книг в классе Program.
+//Выведите на экран консоли значение BooksCount.
+namespace Practice._5._4;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Book book1 = new Book("skazkaocaresoltane1", "Pushkin", 1782, "Skazki");
+        Book book2 = new Book("skazkaocaresoltane2", "Pushkin", 1782, "Skazki");
+        Book book3 = new Book("skazkaocaresoltane3", "Pushkin", 1782, "Skazki");
+
+        Console.WriteLine("Total number of books created: " + Book.BooksCount);
     }
 }
-
