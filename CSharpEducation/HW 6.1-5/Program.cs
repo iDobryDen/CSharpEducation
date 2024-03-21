142bba5527fd281fff6bfda17ae8d71976cb0094
iDobryDen <59179395+iDobryDen@users.noreply.github.com>
2024-03-21 19:23:41 +0400

    продолжение HW.6

--- CSharpEducation/HW 6.1-5/Program.cs
+++ CSharpEducation/HW 6.1-5/Program.cs
@@ -1,10 +1,82 @@
-﻿namespace HW_6._1_5;
+﻿//1. Создать статичный класс Search
+//2. Реализовать статичный метод LinearSearch для поиска в массиве int некоторого числа с помощью линейного поиска.
+//3. Реализовать статичный метод BinarySearch для поиска в массиве int некоторого числа с помощью бинарного поиска.
+//4. Реализовать статичный метод BinarySearch для поиска в массиве string некоторой строки с помощью бинарного поиска.
+//5. Проверьте как работают эти методы в классе Program.
 
-class Program
+namespace HW_6._1_5;
+
+//class Program
+//{
+//    static void Main(string[] args)
+//    {
+//        Console.WriteLine("Hello, World!");
+//    }
+//}
+
+static class Search
 {
-    static void Main(string[] args)
+    public static void Main()
     {
-        Console.WriteLine("Hello, World!");
+        int[] array = new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        int SeekNumber = 6;
+        LinearSearch(array, SeekNumber);
+        BinarySearch(array, SeekNumber);
+    }
+
+    public static void LinearSearch(int[] _array, int _SeekNumber)
+    {
+        int LinearIteration = 0;
+        int SuccessSearch = 0;
+        for (int i = 0; i <= _array.Length; i++)
+        {
+            LinearIteration++;
+            if (_SeekNumber == i)
+            {
+                Console.WriteLine($"Число {_SeekNumber} найдено за {LinearIteration} линейных циклов");
+                SuccessSearch++;
+            }
+        }
+        if (SuccessSearch == 0)
+        {
+            Console.WriteLine($"Число " + _SeekNumber + " не найдено за " + LinearIteration + " линейных циклов");
+        }
+    }
+
+    public static void BinarySearch(int[] _array, int _SeekNumber)
+    {
+        int left_index = 0;
+        int right_index = _array.Length - 1;
+        int middle_index = (left_index + right_index) / 2;
+
+        int BinaryIteration = 0;
+        int SuccessSearch = 0;
+
+        if (left_index > right_index)
+        {
+            Console.WriteLine($"Массив {_array} не отсортирован по возрастанию");
+        }
+
+        while (true)
+        {
+            BinaryIteration++;
+            if (_SeekNumber == middle_index)
+            {
+                Console.WriteLine($"Число {_SeekNumber} найдено за {BinaryIteration} бинарных циклов");
+                SuccessSearch++;
+            }
+            if (_SeekNumber < middle_index)
+            {
+                right_index = middle_index - 1;
+            }
+            if (_SeekNumber > middle_index)
+            {
+                left_index = middle_index + 1;
+            }
+            //if (SuccessSearch == 0)
+            //{
+            //    Console.WriteLine($"Число " + _SeekNumber + " не найдено за " + BinaryIteration + " линейных циклов");
+            //}
+        }
     }
 }
-
---
+++ CSharpEducation/HW 6.1-5/Search.cs
@@ -0,0 +1,32 @@
+﻿//using System;
+//using static System.Runtime.InteropServices.JavaScript.JSType;
+
+//namespace HW_6._1_5;
+
+//static class Search
+//{
+//    public static void Main2()
+//    {
+//       int[] array = new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+//       int SeekNumber = 6;
+//       LinearSearch(array, SeekNumber);
+//    }
+
+//    public static void LinearSearch(int[] array, int SeekNumber)
+//    {
+//        int LinearIteration = 0;
+//        for (int i = 0; i <= array.Length; i++)
+//        {
+//            LinearIteration++;
+//            if (SeekNumber == i)
+//            {
+//                Console.WriteLine($"Число " + SeekNumber + " найдено за " + LinearIteration + " линейных циклов");
+//            }
+//            else
+//            {
+//                Console.WriteLine($"Число " + SeekNumber + " не найдено за " + LinearIteration + " линейных циклов");
+//            }
+//        }
+//    }
+//}
+
