45a0bc6cc675d79b8261b079cdc1429fcdc61e70
iDobryDen <59179395+iDobryDen@users.noreply.github.com>
2024-03-26 01:52:44 +0400

    HW 7.2 create folder binarySearch

---
+++ BinarySearch/BinarySearch/BinarySearch.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace BinarySearch;
+
+/// <summary>
+/// Двоичный поиск в отсортированном массиве.
+/// </summary>
+public static class BinarySearch
+{
+    /// <summary>
+    /// Искать число <see cref="x"/> массиве целочисленных чисел.
+    /// </summary>
+    /// <param name="array">Массив целочисленных чисел.</param>
+    /// <param name="x">Искомый элемент.</param>
+    /// <returns>Индекс <see cref="x"/> в массиве. Иначе <c>-1</c>, если элемент не найден.</returns>
+    public static int Search(int[] array, int x)
+    {
+        // Здесь нужно реализовать поиск.
+        return -1;
+    }
+}
