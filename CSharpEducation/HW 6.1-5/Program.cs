faba00a9331477321c92348319048d6793617c25
iDobryDen <59179395+iDobryDen@users.noreply.github.com>
2024-03-26 04:57:41 +0400

    Task HW 7.4 done, но есть вопрос в строке  131.

--- CSharpEducation/HW 6.1-5/Program.cs
+++ CSharpEducation/HW 6.1-5/Program.cs
@@ -19,9 +19,12 @@ static class Search
     public static void Main()
     {
         int[] array = new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        string[] s_array = new[] { "a", "ab", "abc", "abcd", "abcde", "abcdef", "abcdefg", "abcdefgh", "abcdefghijklmnop" };
         int SeekNumber = 6;
+        string SeekWord = "abcdefgh";
         LinearSearch(array, SeekNumber);
         BinarySearch(array, SeekNumber);
+        BinaryStrSearch(s_array, SeekWord);
     }
 
     public static void LinearSearch(int[] _array, int _SeekNumber)
@@ -80,8 +83,51 @@ static class Search
             //    Console.WriteLine($"Число " + _SeekNumber + " не найдено за " + BinaryIteration + " линейных циклов");
             //}
         }
-        return -1;
+        //return -1;
+    }
+
+    // почему то находит не за 2 бинарных цикла, а за 3, хотя уже на второй итерации middle_index равен _SeekNumber
+
+    public static string BinaryStrSearch(string[] _s_array, string _SeekWord)
+    {
+        int left_index = 0;
+        int right_index = _s_array.Length - 1;
+        string str_left = _s_array[left_index];
+        string str_right = _s_array[right_index];
+        int result = str_left.CompareTo(str_right);
+
+        int BinaryIteration = 0;
+        int SuccessSearch = 0;
+
+        if (result > 0)
+        {
+            Console.WriteLine($"Массив не отсортирован по возрастанию");
+            return "-1";
+        }
+
+        while (true)
+        //while (left_index <= right_index)
+        {
+            int middle_index = (left_index + right_index) / 2;
+            int result2 = _SeekWord.CompareTo(_s_array[middle_index]);
+            BinaryIteration++;
+            if (_SeekWord == _s_array[middle_index])
+            {
+                Console.WriteLine($"слово {_SeekWord} найдено за {BinaryIteration} бинарных циклов");
+                SuccessSearch++;
+                return _s_array[middle_index];
+            }
+            else if (result2 < 0)
+            {
+                right_index = middle_index - 1;
+            }
+            else if (result2 > 0)
+            {
+                left_index = middle_index + 1;
+            }
+        }
+        Console.WriteLine($"Слово {_SeekWord} не найдено");
+        return "-1";
+        // строка 108-109, не разобрался с условием while, почему если true, то есть недостижимый код в строках 129-130?
     }
 }
-
-// почему то находит не за 2 бинарных цикла, а за 3, хотя уже на второй итерации middle_index равен _SeekNumber
